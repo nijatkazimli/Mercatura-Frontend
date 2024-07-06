6e3e9710c9afad4122cd0bea08fb8ea6715e0e52
nctkzmli <nctkzmli@gmail.com>
2024-07-06 15:08:17 +0200

    not rendering instead

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LeftBar from "../LeftBar/LeftBar";
 import "./Home.css";
+import { isMobile } from "react-device-detect";
 
 const Home = () => {
     return (
         <div className="layout">
-            <LeftBar />
+            { !isMobile && <LeftBar /> }
             <div className="page-content">
                 <p>
                     Products

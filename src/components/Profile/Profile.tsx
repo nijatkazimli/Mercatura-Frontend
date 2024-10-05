08abf316f972d552b30f44d413238d4c1afce5df
nijatkazimliPG <kazimli.n@pg.com>
2024-10-05 10:59:15 +0200

    being able to change profile photo, styling

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, Flex, TextField } from '@radix-ui/themes';
 import { Text } from '@radix-ui/themes/dist/cjs/components/callout';
 import { useNavigate } from 'react-router-dom';
@@ -7,15 +7,28 @@ import { selectUser } from '../../redux/selectors';
 import './Profile.css';
 import Images from '../../constants/Images';
 import AuthContext from '../../hooks/AuthContext';
+import FileUploadPopup from '../General/FileUploadPopup/FileUploadPopup';
+import { postImage } from '../../api';
+import { getUser } from '../../redux/actions';
 
 function Profile() {
   const user = useSelector(selectUser);
   const { authResponse, setAuthResponse } = useContext(AuthContext);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const logOut = () => {
     setAuthResponse(null);
     navigate('/');
+  };
+
+  const onProfilePhotoUpload = async (profilePicture: File[]) => {
+    if (user && authResponse?.token) {
+      const { token } = authResponse;
+      const { id } = user;
+      await postImage(`/user/${id}/image`, profilePicture[0], token);
+      dispatch(getUser({ userId: id, token }));
+    }
   };
 
   return (
@@ -27,9 +40,21 @@ function Profile() {
             alt={Images.defaultProfilePicture.alt}
             className="profile-image"
           />
-          <Button onClick={logOut} color="red" size="3" style={{ maxWidth: 120 }}>
-            Log Out
-          </Button>
+          <Flex direction="row" gap="3">
+            <FileUploadPopup
+              trigger={(
+                <Button color="purple" size="3" style={{ maxWidth: 200 }}>
+                  Change Profile Picture
+                </Button>
+              )}
+              title="Change your profile picture"
+              multiple={false}
+              onUpload={onProfilePhotoUpload}
+            />
+            <Button onClick={logOut} color="red" size="3" style={{ maxWidth: 120 }}>
+              Log Out
+            </Button>
+          </Flex>
         </Flex>
         <Flex direction="column" gap="2" ml="4">
           <Text size="7">

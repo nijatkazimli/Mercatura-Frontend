55d4ea70c93825d3837dc2e31f67740ebcb6f4e8
nijatkazimliPG <kazimli.n@pg.com>
2024-10-09 23:45:02 +0200

    prettier

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -24,7 +24,9 @@ function ErrorPage() {
   return (
     <div style={styles.container}>
       <h1 style={styles.heading}>404</h1>
-      <p style={styles.message}>Oops! The page you&#39;re looking for doesn&#39;t exist.</p>
+      <p style={styles.message}>
+        Oops! The page you&#39;re looking for doesn&#39;t exist.
+      </p>
       <NavLink to="/">Go back to Home</NavLink>
     </div>
   );
